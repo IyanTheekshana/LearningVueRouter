2a8cf5e157ff3ec20f99ef58f19d50ae0bc53893
Iyan Theekshana <iyan.dev123@gmail.com>
2023-03-07 10:35:28 +0100

    added rouuter npm i --save vue router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
+
+app.mount("#app");
